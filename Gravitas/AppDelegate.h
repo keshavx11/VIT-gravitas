74fa0fa828a86d2859540270c722c1a645b22f98
Keshav Bansal <keshav_x11@yahoo.co.in>
2015-07-17 00:19:10 +0530

    Initial Commit

---
+++ Gravitas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gravitas
+//
+//  Created by Keshav Bansal on 17/07/15.
+//  Copyright (c) 2015 Keshav Bansal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gravitas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gravitas
+//
+//  Created by Keshav Bansal on 17/07/15.
+//  Copyright (c) 2015 Keshav Bansal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
